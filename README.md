70e696193b239f4a4bb23dd1f4e7888e6e3bd172
Phuc <ngovanphuc@hotmail.com>
2020-10-17 09:31:14 +0700

    init project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Simple implementation of MVC pattern for training reference
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+
+$app = $app = require_once __DIR__.'/../bootstrap/app.php';
